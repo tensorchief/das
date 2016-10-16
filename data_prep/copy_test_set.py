0efab68aa31aea93b762de012538d70f851228a1
rsb_cscs <silvia.b.reynolds@gmail.com>
2016-10-16 09:01:16 +0200

    added file to prepare test set

---
+++ data_prep/copy_test_set.py
@@ -0,0 +1,22 @@
+#! /apps/escha/UES/RH6.7/easybuild/software/Python/3.5.0-gmvolf-15.11/bin/python
+
+import glob
+import os
+from shutil import copyfile
+import re
+
+region = 310
+
+with open('training_set.txt', 'r') as infile:
+    training_runs = infile.readlines()
+
+print(training_runs)
+destination = '/users/rsb/Training_set/'
+
+for run in training_runs:
+    files = glob.glob(run.strip() + '/training_*' + str(region) + '*.npy')
+    for item in files:
+        print(item)
+        file_name = re.match('.*/(training_.*\.npy)',item).group(1)
+        print(file_name)
+        copyfile(item,destination+file_name)
