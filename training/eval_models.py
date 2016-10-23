adfd738d7dcdeb8fdc2080c7a3ed60a60607bdcf
Silvia Reynolds <silvia.b.reynolds@gmail.com>
2016-10-23 19:56:32 +0200

    initial model evaluation

---
+++ training/eval_models.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+import tensorflow
+
+import glob
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+def construct_np_arrays(runs):
+    """
+    Numpy files of the runs given are loaded and model/label sets are
+    concatenated
+    :param runs: list of model runs (e.g. of training set)
+    :returns: model_data_np (X), labels_np (Y)
+    """
+    model_data_np = np.empty((0,28,28,21),int)
+    labels_np = np.empty((0,2),int)
+
+    for item in runs:
+        # load model data
+        cur_model = np.load(item)
+        model_data_np = np.concatenate((model_data_np,cur_model),axis = 0)
+
+        # load matching labels
+        cur_index = re.match('.*([0-9]{8}_[0-9]{3}.*)',item).group(1)
+        cur_label = np.load(datdir + 'training_labels_' + cur_index)
+        labels_np = np.concatenate((labels_np,cur_label),axis = 0)
+
+    return model_data_np, labels_np
+
+def replace_entries(np_array):
+    """
+    converts numpy array of shape (-1,2) to (-1,1)
+    :param np_array: numpy_array (-1,2)
+    :returns: list (-1,1)
+    """
+    output = list()
+    for item in np_array:
+        output.append(item[1])
+    return output
+
+def setup_cnn():
+    """
+    defines model architecture of a cnn
+    :param void:
+    :return: model
+    """
+    network = input_data(shape=[None, 28, 28, 21], name='input')
+    network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
+    #network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
+    network = max_pool_2d(network, 2)
+
+    network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
+    #network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
+    network = max_pool_2d(network, 2)
+
+    network = fully_connected(network, 128, activation='tanh')
+    network = dropout(network, 0.8)
+    network = fully_connected(network, 256, activation='tanh')
+    network = dropout(network, 0.8)
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=0.001,
+                 loss='categorical_crossentropy', name='target')
+    return tflearn.DNN(network, tensorboard_verbose=0)
+
+
+region = 144
+
+# Data loading & preprocessing
+datdir = '/home/silviar/Dokumente/Test_set/'
+test_files = sorted(glob.glob(datdir + 'training_data_*'))
+
+X,Y = construct_np_arrays(test_files)
+
+model = setup_cnn()
+
+# load weights
+model.load('/home/silviar/Dokumente/Abschlussarbeit/training/models/' \
+            + 'cnn_mnist_stratified_' + str(region))
+
+# do predictions
+pred = model.predict(X)
+predicted_label = [item.index(max(item)) for item in pred]
+valid_label = [list(item).index(max(item)) for item in Y]
+
+# do evaluations
+print('ROC/AUC: ',tflearn.objectives.roc_auc_score(predicted_label,valid_label))
+
+# calculate scores
+print('Accuracy: ', accuracy_score(valid_label,predicted_label))
+print('Confusion matrix: ', confusion_matrix(valid_label,predicted_label))
+
