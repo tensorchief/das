a119f637d416749d7641cf711d78a8fa240d986e
rsb_cscs <silvia.b.reynolds@gmail.com>
2016-10-23 13:06:31 +0200

    test set prep

--- data_prep/copy_test_set.py
+++ data_prep/copy_test_set.py
@@ -4,19 +4,32 @@ import glob
 import os
 from shutil import copyfile
 import re
+import argparse
 
-region = 310
+p = argparse.ArgumentParser()
+p.add_argument("region")
+args = p.parse_args()
+region = args.region
 
 with open('training_set.txt', 'r') as infile:
-    training_runs = infile.readlines()
+    training_data = infile.readlines()
 
-print(training_runs)
-destination = '/users/rsb/Training_set/'
+training_runs = [item.strip() for item in training_data]
 
-for run in training_runs:
-    files = glob.glob(run.strip() + '/training_*' + str(region) + '*.npy')
-    for item in files:
-        print(item)
-        file_name = re.match('.*/(training_.*\.npy)',item).group(1)
-        print(file_name)
-        copyfile(item,destination+file_name)
+all_runs = glob.glob('/scratch/rsb/Test/*COSMO_E')
+destination = '/users/rsb/Test_set/'
+
+for run in all_runs:
+    if run not in training_runs:
+        files = glob.glob(run.strip() + '/training_*' + str(region) + '*.npy')
+        for item in files:
+            print(item)
+            file_name = re.match('.*/(training_.*\.npy)',item).group(1)
+            print(file_name)
+            copyfile(item,destination+file_name)
+        files = glob.glob(run.strip() + '/benchmark_*_*_' + str(region) + '*.npy')
+        for item in files:
+            print(item)
+            file_name = re.match('.*/(benchmark_.*\.npy)',item).group(1)
+            print(file_name)
+            copyfile(item,destination+file_name)
