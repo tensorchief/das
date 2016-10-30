881cd7bcf45f90827c074b76a0bb535cba6d5374
Silvia Reynolds <silvia.b.reynolds@gmail.com>
2016-10-30 12:26:31 +0100

    added region argparse & performed initial roc analyses

--- training/eval_models.py
+++ training/eval_models.py
@@ -12,6 +12,7 @@ from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
 import tensorflow
 
+import argparse
 import glob
 import numpy as np
 import re
@@ -19,7 +20,7 @@ import re
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def construct_np_arrays(runs, model_data_np):
+def construct_np_arrays(datdir, runs, model_data_np):
     """
     Numpy files of the runs given are loaded and model/label sets are
     concatenated
@@ -35,7 +36,7 @@ def construct_np_arrays(runs, model_data_np):
 
         # load matching labels
         cur_index = re.match('.*([0-9]{8}_[0-9]{3}.*)',item).group(1)
-        cur_tag = re.match('.*/([a-z]+)_data_[0-9]{8}_[0-9]{3}.*',item).group(1)
+        cur_tag = re.match('.*/([a-z]+)_[a-z]+_[0-9]{8}_[0-9]{3}.*',item).group(1)
         cur_label = np.load(datdir + cur_tag + '_labels_' + cur_index)
         labels_np = np.concatenate((labels_np,cur_label),axis = 0)
 
@@ -76,64 +77,69 @@ def setup_cnn():
                  loss='categorical_crossentropy', name='target')
     return tflearn.DNN(network, tensorboard_verbose=0)
 
+def main(region):
 
-region = 310
+    # Data loading & preprocessing
+    datdir = '/home/silviar/Dokumente/Test_set/'
+    test_files = sorted(glob.glob(datdir + 'training_data_*'))
 
-# Data loading & preprocessing
-datdir = '/home/silviar/Dokumente/Test_set/'
-test_files = sorted(glob.glob(datdir + 'training_data_*'))
+    X,Y = construct_np_arrays(datdir,test_files,np.empty((0,28,28,21),int))
 
-X,Y = construct_np_arrays(test_files,np.empty((0,28,28,21),int))
+    model = setup_cnn()
 
-model = setup_cnn()
+    # load weights
+    model.load('/home/silviar/Dokumente/Abschlussarbeit/training/models/' \
+                + 'cnn_mnist_stratified_' + str(region))
 
-# load weights
-model.load('/home/silviar/Dokumente/Abschlussarbeit/training/models/' \
-            + 'cnn_mnist_stratified_' + str(region))
+    # do predictions
+    pred = model.predict(X)
+    predicted_label = [item.index(max(item)) for item in pred]
+    valid_label = [item.argmax() for item in Y]
+    y_score = np.array(pred)
 
-# do predictions
-pred = model.predict(X)
-predicted_label = [item.index(max(item)) for item in pred]
-valid_label = [item.argmax() for item in Y]
-y_score = np.array(pred)
+    # calculate scores
+    print('Accuracy: ', accuracy_score(valid_label,predicted_label))
+    print('Confusion matrix: ', confusion_matrix(valid_label,predicted_label))
 
-# get benchmark
-benchmark_files = sorted(glob.glob(datdir + 'benchmark_data_*'))
-pred_bench,Y_bench = construct_np_arrays(benchmark_files,np.empty((0,2),int))
+    # calculate roc curve
+    fpr, tpr, _ = roc_curve(Y[:,0],y_score[:,0])
+    roc_auc = auc(fpr,tpr)
 
-predicted_bench = [item.argmax() for item in pred_bench]
-valid_bench = [item.argmax() for item in Y_bench]
+    """
+    # get benchmark
+    benchmark_files = sorted(glob.glob(datdir + 'benchmark_prob_*'))
+    pred_bench,Y_bench = construct_np_arrays(datdir,benchmark_files,np.empty((0,2),int))
+    predicted_label_bench = [item.argmax() for item in pred_bench]
+    valid_label_bench = [item.argmax() for item in Y_bench]
 
-# do evaluations
-#print('ROC/AUC: ',tflearn.objectives.roc_auc_score(pred,valid_label))
-print('ROC/AUC: ',roc_auc_score(valid_label,predicted_label))
+    # calculate scores
+    print('Accuracy: ', accuracy_score(valid_label_bench,predicted_label_bench))
+    print('Confusion matrix: ', confusion_matrix(valid_label_bench,predicted_label_bench))
 
-# calculate scores
-print('Accuracy: ', accuracy_score(valid_label,predicted_label))
-print('Confusion matrix: ', confusion_matrix(valid_label,predicted_label))
+    # calculate roc_curve
+    fpr_bench, tpr_bench, _ = roc_curve(Y_bench[:,0],pred_bench[:,0])
+    roc_auc_bench = auc(fpr_bench,tpr_bench)
+    """
 
-# calculate roc curve
-fpr = list()
-tpr = list()
-#fpr, tpr, _ = roc_curve(Y.ravel(),y_score.ravel())
-#roc_auc = auc(fpr,tpr)
+    outdir = '/home/silviar/Dokumente/Abschlussarbeit/training/models/'
+    plt.figure()
+    plt.plot(fpr,tpr, color = 'deeppink', lw = 2, label = 'class 1 (area = %0.2f)'\
+            % roc_auc)
+    #plt.plot(fpr_bench,tpr_bench, color = 'purple', lw = 2, \
+    #        label = 'Benchmark (area = %0.2f)' % roc_auc_bench)
+    plt.plot([0,1],[0,1], color = 'navy', lw = 2, linestyle = '--')
+    plt.xlim([0.0,1.0])
+    plt.ylim([0.0,1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC curve for region ' + str(region))
+    plt.legend(loc='lower right')
+    plt.savefig(outdir + 'roc_' + str(region) + '.png')
 
-fpr,tpr,_ = roc_curve(valid_label,predicted_label)
-roc_auc = auc(fpr,tpr)
+if __name__ == "__main__":
+    # get Region
+    p = argparse.ArgumentParser()
+    p.add_argument("region")
+    args = p.parse_args()
 
-fpr_bench,tpr_bench,_ = roc_curve(valid_bench,predicted_bench)
-roc_auc_bench = auc(fpr_bench,tpr_bench)
-
-plt.figure()
-plt.plot(fpr,tpr, color = 'deeppink', lw = 2, label = 'ROC curve (area = %0.2f)'\
-        % roc_auc)
-plt.plot(fpr_bench,tpr_bench, color = 'purple', lw = 2, \
-        label = 'Benchmark (area = %0.2f)' % roc_auc_bench)
-plt.plot([0,1],[0,1], color = 'navy', lw = 2, linestyle = '--')
-plt.xlim([0.0,1.0])
-plt.ylim([0.0,1.0])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC curve for region ' + str(region))
-plt.legend(loc='lower right')
-plt.show()
+    main(int(args.region))
