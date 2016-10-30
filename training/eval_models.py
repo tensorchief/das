de15ad23ec188f3bf22acfa579cce6f532f4da74
Silvia Reynolds <silvia.b.reynolds@gmail.com>
2016-10-30 08:59:15 +0100

    Roc eval contd.

--- training/eval_models.py
+++ training/eval_models.py
@@ -15,18 +15,17 @@ import tensorflow
 import glob
 import numpy as np
 import re
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def construct_np_arrays(runs):
+def construct_np_arrays(runs, model_data_np):
     """
     Numpy files of the runs given are loaded and model/label sets are
     concatenated
     :param runs: list of model runs (e.g. of training set)
     :returns: model_data_np (X), labels_np (Y)
     """
-    model_data_np = np.empty((0,28,28,21),int)
     labels_np = np.empty((0,2),int)
 
     for item in runs:
@@ -36,7 +35,8 @@ def construct_np_arrays(runs):
 
         # load matching labels
         cur_index = re.match('.*([0-9]{8}_[0-9]{3}.*)',item).group(1)
-        cur_label = np.load(datdir + 'training_labels_' + cur_index)
+        cur_tag = re.match('.*/([a-z]+)_data_[0-9]{8}_[0-9]{3}.*',item).group(1)
+        cur_label = np.load(datdir + cur_tag + '_labels_' + cur_index)
         labels_np = np.concatenate((labels_np,cur_label),axis = 0)
 
     return model_data_np, labels_np
@@ -77,13 +77,13 @@ def setup_cnn():
     return tflearn.DNN(network, tensorboard_verbose=0)
 
 
-region = 144
+region = 310
 
 # Data loading & preprocessing
 datdir = '/home/silviar/Dokumente/Test_set/'
 test_files = sorted(glob.glob(datdir + 'training_data_*'))
 
-X,Y = construct_np_arrays(test_files)
+X,Y = construct_np_arrays(test_files,np.empty((0,28,28,21),int))
 
 model = setup_cnn()
 
@@ -94,8 +94,15 @@ model.load('/home/silviar/Dokumente/Abschlussarbeit/training/models/' \
 # do predictions
 pred = model.predict(X)
 predicted_label = [item.index(max(item)) for item in pred]
-valid_label = [list(item).index(max(item)) for item in Y]
+valid_label = [item.argmax() for item in Y]
 y_score = np.array(pred)
+
+# get benchmark
+benchmark_files = sorted(glob.glob(datdir + 'benchmark_data_*'))
+pred_bench,Y_bench = construct_np_arrays(benchmark_files,np.empty((0,2),int))
+
+predicted_bench = [item.argmax() for item in pred_bench]
+valid_bench = [item.argmax() for item in Y_bench]
 
 # do evaluations
 #print('ROC/AUC: ',tflearn.objectives.roc_auc_score(pred,valid_label))
@@ -108,12 +115,20 @@ print('Confusion matrix: ', confusion_matrix(valid_label,predicted_label))
 # calculate roc curve
 fpr = list()
 tpr = list()
-fpr, tpr, _ = roc_curve(Y.ravel(),y_score.ravel())
+#fpr, tpr, _ = roc_curve(Y.ravel(),y_score.ravel())
+#roc_auc = auc(fpr,tpr)
+
+fpr,tpr,_ = roc_curve(valid_label,predicted_label)
 roc_auc = auc(fpr,tpr)
+
+fpr_bench,tpr_bench,_ = roc_curve(valid_bench,predicted_bench)
+roc_auc_bench = auc(fpr_bench,tpr_bench)
 
 plt.figure()
 plt.plot(fpr,tpr, color = 'deeppink', lw = 2, label = 'ROC curve (area = %0.2f)'\
         % roc_auc)
+plt.plot(fpr_bench,tpr_bench, color = 'purple', lw = 2, \
+        label = 'Benchmark (area = %0.2f)' % roc_auc_bench)
 plt.plot([0,1],[0,1], color = 'navy', lw = 2, linestyle = '--')
 plt.xlim([0.0,1.0])
 plt.ylim([0.0,1.0])
