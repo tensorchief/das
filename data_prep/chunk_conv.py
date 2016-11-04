e99e0e195e0cc9cd418a4e2b7be7d94d797dd606
rsb_cscs <silvia.b.reynolds@gmail.com>
2016-11-04 15:19:26 +0100

    Extended Test set

--- data_prep/chunk_conv.py
+++ data_prep/chunk_conv.py
@@ -7,16 +7,18 @@ import math
 import argparse
 
 def get_subdir(directory):
-	'''
-	returns list of sub-subdirectories of the input variable directory
-	'''
-	subdir = list()
-	for dir in os.listdir(directory):
-		subdir.extend([directory+dir+'/'+item+'/grib/' for item in os.listdir(directory+dir) if re.match('[0-9]{8}_[0-9]{3}',item)])
-	return subdir
+    '''
+    returns list of sub-subdirectories of the input variable directory
+    '''
+    subdir = list()
+    for dir in [item for item in os.listdir(directory) if re.match('FCST[0-9]{2}',item) \
+                or re.match('LM[0-9]{2}',item)]:
+        subdir.extend([directory+dir+'/'+item+'/grib/' for item in os.listdir(directory+dir) \
+                    if re.match('[0-9]{8}_[0-9]{3}',item)])
+    return subdir
 
 
-def print_namelist(path,template):
+def print_namelist(path,template,target):
 	'''
 	writes a fieldextra namelist that converts files of pattern
 	'''
@@ -29,7 +31,7 @@ def print_namelist(path,template):
 	# preallocate in and outfile names
 	date = re.match('.+/([0-9]{8}).+',path).group(1)
 	infile = path + pattern
-	outfile_nc = date + '_' + pattern
+	outfile_nc = target + '/' + date + '_' + pattern
 	
 	# write fieldextra file according to template
 	filename_fx = 'fieldextra_'+date
@@ -53,10 +55,10 @@ def convert(namelist):
 	'''
 	p = Popen(['/oprusers/owm/opr/abs/fieldextra', namelist])
 	p.wait()
-	os.remove(namelist)
+#	os.remove(namelist)
 
 # set number of chunks for parallel processing
-num_chunks = 24
+num_chunks = 20
 
 # get fieldextra pattern
 fx_file = 'fieldextra_nl'
@@ -65,11 +67,12 @@ fx_lines = file_obj.readlines()
 file_obj.close()
 
 # get locators
-rootdir = ['/store/s83/tsm/EXP_TST/590/', '/store/s83/owm/COSMO-E/']
+#rootdir = ['/store/s83/tsm/EXP_TST/590/', '/store/s83/owm/COSMO-E/']
+rootdir = ['/store/s83/owm/COSMO-E/']
 locator = list()
 
 for dir in rootdir:
-	locator.extend(get_subdir(dir))
+    locator.extend(get_subdir(dir))
 
 # get runs that have already been calculated
 done = sorted([re.match('([0-9]+)_COSMO_E',item).group(1) for item in os.listdir('/scratch/rsb/Test/') if re.match('([0-9]+)_COSMO_E',item)])
@@ -78,8 +81,7 @@ done = sorted([re.match('([0-9]+)_COSMO_E',item).group(1) for item in os.listdir
 locator_new = [locator[id] for id in range(1,len(locator))\
  if not any(re.match('.+' + done_item + '.+', locator[id]) for done_item in done)\
  and re.match('.+([0-9]{8}).+',locator[id]).group(1) != re.match('.+([0-9]{8}).+',locator[id-1]).group(1)]
-
-locator_new.append(locator[0])
+#locator_new.append(locator[0])
 
 # split the runs into num_chunks partitions
 ids = math.ceil(len(locator_new)/num_chunks)
@@ -93,8 +95,11 @@ chunk = int(args.chunk)
 
 # conversion
 for item in chunks[chunk]:
-	# create namelist
-	#item = locator_new[10]
-	namelist_fx = print_namelist(item,fx_lines)
-	# do the conversion
-	convert(namelist_fx)
+    # create directory
+    date = re.match('.+/([0-9]{8}).+',item).group(1)
+    target_dir = '/scratch/rsb/Test/' + date + '_COSMO_E'
+    os.mkdir(target_dir)
+    # create namelist
+    namelist_fx = print_namelist(item,fx_lines,target_dir)
+    # do the conversion
+    convert(namelist_fx)
--- data_prep/run_chunks.sh
+++ data_prep/run_chunks.sh
@@ -3,7 +3,8 @@
 for i in $(seq 0 19)
 do
 	#echo "$i"
-    batchPP -t 16 ./crop_netcdf_24hours_benchmark.py $i
+    batchPP -t 12 ./chunk_conv.py $i
+    #batchPP -t 16 ./crop_netcdf_24hours_benchmark.py $i
 	#batchPP -t 24 -p pp-long ./crop_netcdf_24hours_II.py $i
 	#./chunk_list.py $i > "outfile$i.txt"
 done
